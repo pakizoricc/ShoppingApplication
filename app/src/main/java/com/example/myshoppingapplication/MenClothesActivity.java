b911b82a1c74a2d81ce0aa8eea38266bbbae90cf
Pavle Zorić <97367273+pakizoricc@users.noreply.github.com>
2023-08-30 02:08:58 +0200

    myproject xD

--- app/src/main/java/com/example/myshoppingapplication/MenClothesActivity.java
+++ app/src/main/java/com/example/myshoppingapplication/MenClothesActivity.java
@@ -1,12 +1,56 @@
 package com.example.myshoppingapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenClothesActivity extends AppCompatActivity {
+    ViewFlipper flipper;
+    Button prev_Button, next_Button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_men_clothes);
+
+        flipper = findViewById(R.id.view_flipper);
+        prev_Button = findViewById(R.id.prev_button);
+        next_Button = findViewById(R.id.next_button);
+
+        prev_Button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        // It is used to set the in and out
+                        // animation of View flipper.
+                        flipper.setInAnimation(MenClothesActivity.this,
+                                R.anim.slide_right);
+                        flipper.setOutAnimation(MenClothesActivity.this,
+                                R.anim.slide_left);
+
+                        // It shows previous item.
+                        flipper.showPrevious();
+                    }
+                });
+
+        next_Button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        // It is used to set the in and out
+                        // animation of View flipper.
+                        flipper.setInAnimation(MenClothesActivity.this,
+                                R.anim.slide_left);
+                        flipper.setOutAnimation(MenClothesActivity.this,
+                                R.anim.slide_right);
+
+                        // It shows next item.
+                        flipper.showNext();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/myshoppingapplication/WomenClothesActivity.java
+++ app/src/main/java/com/example/myshoppingapplication/WomenClothesActivity.java
@@ -1,7 +1,54 @@
 package com.example.myshoppingapplication;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WomenClothesActivity extends AppCompatActivity {
+    ViewFlipper flipper;
+    Button prev_Button, next_Button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_women_clothes);
 
+        flipper = findViewById(R.id.view_flipper);
+        prev_Button = findViewById(R.id.prev_button);
+        next_Button = findViewById(R.id.next_button);
+
+        prev_Button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // It is used to set the in and out
+                        // animation of View flipper.
+                        flipper.setInAnimation(WomenClothesActivity.this,
+                                R.anim.slide_right);
+                        flipper.setOutAnimation(WomenClothesActivity.this,
+                                R.anim.slide_left);
+
+                        // It shows previous item.
+                        flipper.showPrevious();
+                    }
+                });
+
+        next_Button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // It is used to set the in and out
+                        // animation of View flipper.
+                        flipper.setInAnimation(WomenClothesActivity.this,
+                                R.anim.slide_left);
+                        flipper.setOutAnimation(WomenClothesActivity.this,
+                                R.anim.slide_right);
+
+                        // It shows next item.
+                        flipper.showNext();
+                    }
+                });
+    }
 }
